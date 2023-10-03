d87fa0ab26c57e9d3ca25751e0493b665fdfd323
aurorg <2519652258@qq.com>
2023-10-03 20:14:25 +0800

    1

---
+++ algorithm_s/src/main/java/tree/Solution106.java
@@ -0,0 +1,56 @@
+package tree;
+
+import java.util.HashMap;
+
+public class Solution106 {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+
+        TreeNode(int val){
+            this.val=val;
+        }
+
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val = val;
+            this.left=left;
+            this.right=right;
+        }
+    }
+
+    HashMap<Integer,Integer> valToIndex = new HashMap<>();
+
+    public TreeNode buildTree(int[] inorder,int[] postorder){
+        for(int i=0;i<inorder.length;i++){
+            valToIndex.put(inorder[i],i);
+        }
+
+        return build(inorder,0,inorder.length-1,
+                postorder,0,postorder.length-1);
+    }
+
+    TreeNode build(int[] inorder,int inStart,int inEnd,
+                   int[] postorder,int postStart,int postEnd){
+        if(inStart>inEnd){
+            return null;
+        }
+
+        int rootVal =postorder[postEnd];
+
+        int index =valToIndex.get(rootVal);
+
+        int leftSize =index-inStart;
+
+        TreeNode root=new TreeNode(rootVal);
+
+        root.left=build(inorder,inStart,index-1,
+                postorder,postStart,postStart+leftSize-1);
+        root.right=build(inorder,index+1,inEnd,
+                postorder,postStart+leftSize,postEnd-1);
+
+        return root;
+    }
+
+}
--- algorithm_s/src/main/java/treetest/solution105.java
+++ algorithm_s/src/main/java/treetest/solution105.java
@@ -5,58 +5,55 @@ import java.util.HashMap;
 
 public class solution105 {
 
-     public static class TreeNode{
-         int val;
-         TreeNode left;
-         TreeNode right;
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){
 
-         TreeNode(){}
+        }
 
-         TreeNode(int val){
-             this.val=val;
-         }
+        TreeNode(int val){
+            this.val=val;
+        }
 
-         TreeNode(int val,TreeNode left, TreeNode right){
-             this.val=val;
-             this.left=left;
-             this.right=right;
-         }
-     }
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val=val;
+            this.left=left;
+            this.right=right;
+        }
+    }
 
-     HashMap<Integer,Integer> valToIndex = new HashMap<>();
+    //中序
+    HashMap<Integer,Integer> valToIndex = new HashMap<>();
 
-     public TreeNode buildTRee(int[] preorder,int[] inorder){
-         for(int i=0;i<inorder.length;i++){
-             valToIndex.put(inorder[i],i);
-         }
-         return build(preorder,0,preorder.length-1,
-                 inorder,0,inorder.length-1);
-     }
+    public TreeNode buildTree(int[] preorder,int[] inorder){
+        for(int i=0;i<inorder.length;i++){
+            valToIndex.put(inorder[i],i);
+        }
+        return build(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
+    }
 
-     TreeNode build(int[] preorder,int preStart,int preEnd,
-                    int[] inorder,int inStart,int inEnd){
-       if(preStart >preEnd){
-           return null;
-       }
+    TreeNode build(int[] preorder,int preStart,int preEnd,
+                   int[] inorder,int inStart,int inEnd){
+        if(preStart > preEnd){
+            return null;
+        }
 
-       int rootVal = preorder[preStart];
+        int rootVal = preorder[preStart];
+        int index =valToIndex.get(rootVal);
 
-       int index = valToIndex.get(rootVal);
+        int leftSize =index -inStart;
 
-       int leftSize =index -inStart;
+        TreeNode root = new TreeNode(rootVal);
 
-       TreeNode root = new TreeNode(rootVal);
+        root.left = build(preorder,preStart+1,preStart + leftSize,
+                inorder,inStart,index-1);
+        root.right = build(preorder,preStart + leftSize +1,preEnd,
+                inorder,index+1,inEnd);
 
-       root.left = build(preorder,preStart+1,preStart+leftSize,
-               inorder,inStart,index-1);
-
-       root.right = build(preorder,preStart+leftSize+1,preEnd,
-               inorder,index+1,inEnd);
-       return root;
-
-     }
-
-
-
+        return root;
+    }
 
 }
